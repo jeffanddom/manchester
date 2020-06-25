bbb59b096f7989603a5f85eb420aeaefbe1d95fd
Jeff Lee <jeffomatic@gmail.com>
2020-06-25 09:27:50 -0700

    refactor debug draw a little

--- src/systems/damager.ts
+++ src/systems/damager.ts
@@ -1,12 +1,29 @@
+import { vec2 } from 'gl-matrix'
+
 import { Game } from '~/Game'
+import { Primitive } from '~/renderer/interfaces'
 
 export const update = (g: Game): void => {
   for (const id in g.entities.entities) {
     const e = g.entities.entities[id]
-    if (!e.transform || !e.damagerLogic) {
+    const transform = e.transform
+    const damagerLogic = e.damagerLogic
+    if (!transform || !damagerLogic) {
       continue
     }
 
-    e.damagerLogic.update(e.transform, id, g)
+    damagerLogic.update(transform, id, g)
+
+    g.debugDraw(() => {
+      const aabb = damagerLogic.aabb(transform)
+      const d = vec2.sub(vec2.create(), aabb[1], aabb[0])
+
+      return {
+        primitive: Primitive.RECT,
+        strokeStyle: 'magenta',
+        pos: aabb[0],
+        dimensions: d,
+      }
+    })
   }
 }
