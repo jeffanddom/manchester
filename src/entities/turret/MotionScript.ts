3db414c8af24e6ffb9a4162637cbfa75949cfcfd
Jeff Lee <jeffomatic@gmail.com>
2020-07-08 21:14:13 -0700

    cleanup rotator interface

--- src/entities/turret/MotionScript.ts
+++ src/entities/turret/MotionScript.ts
@@ -1,6 +1,7 @@
 import { IMotionScript } from '~/entities/components/interfaces'
 import { Transform } from '~/entities/components/Transform'
 import { Game } from '~/Game'
+import { getAngle } from '~/util/math'
 import { rotate } from '~/util/rotator'
 
 const TURRET_ROT_SPEED = Math.PI / 2
@@ -8,10 +9,9 @@ const TURRET_ROT_SPEED = Math.PI / 2
 export class MotionScript implements IMotionScript {
   update(transform: Transform, _entityId: string, g: Game, dt: number): void {
     transform.orientation = rotate({
-      from: transform,
-      to: g.player.unwrap().transform!.position,
-      speed: TURRET_ROT_SPEED,
-      dt,
+      from: transform.orientation,
+      to: getAngle(transform.position, g.player.unwrap().transform!.position),
+      amount: TURRET_ROT_SPEED * dt,
     })
   }
 }
