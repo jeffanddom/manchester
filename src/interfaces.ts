f496f59bb98f812b17fb8a108e8e3e168b270d00
Jeff Lee <jeffomatic@gmail.com>
2020-07-08 21:20:19 -0700

    remove superfluous interface

--- src/interfaces.ts
+++ src/interfaces.ts
@@ -1,13 +1,6 @@
-import { vec2 } from 'gl-matrix'
-
 export enum Direction {
   North = 'N',
   South = 'S',
   East = 'E',
   West = 'W',
-}
-
-export interface TransformData {
-  orientation: number
-  position: vec2
 }
