097d61d903843df011edb1314883648ccc382600
Jeff Lee <jeffomatic@gmail.com>
2020-07-25 18:16:50 -0700

    PriorityQueue: new class (#41)
    PriorityQueue is a new utility class that can be used to optimize
    pathfinding and prevent redundant sorting.

---
+++ src/util/PriorityQueue.test.ts
@@ -0,0 +1,69 @@
+import { shuffle } from 'lodash'
+
+import { PriorityQueue } from './PriorityQueue'
+
+const permutations = <T>(values: Array<T>): Array<Array<T>> => {
+  if (values.length === 0) {
+    return []
+  }
+
+  if (values.length === 1) {
+    return [values.slice()]
+  }
+
+  const first = values[0]
+  const restPerms = permutations(values.slice(1))
+  const perms: Array<Array<T>> = []
+
+  restPerms.forEach((prevPerm) => {
+    for (let insertAt = 0; insertAt < values.length; insertAt++) {
+      const newPerm = []
+      let prevN = 0
+
+      while (newPerm.length < values.length) {
+        if (newPerm.length === insertAt) {
+          newPerm.push(first)
+          continue
+        }
+
+        newPerm.push(prevPerm[prevN])
+        prevN++
+      }
+
+      perms.push(newPerm)
+    }
+  })
+
+  return perms
+}
+
+for (let testSize = 1; testSize < 6; testSize++) {
+  const vals = [...Array(testSize).keys()]
+  permutations(vals).forEach((perm) => {
+    test(`pushing ${perm} should result in ${vals}`, () => {
+      const pq = new PriorityQueue<number>((a, b) => a - b)
+      perm.forEach((n) => pq.push(n))
+
+      const got: Array<number> = []
+      while (pq.length() !== 0) {
+        got.push(pq.pop()!)
+      }
+
+      expect(got).toStrictEqual(vals)
+    })
+  })
+}
+
+test('works with huge inputs', () => {
+  const vals = [...new Array(2048).keys()]
+
+  const pq = new PriorityQueue<number>((a, b) => a - b)
+  shuffle(vals).forEach((n) => pq.push(n))
+
+  const got: Array<number> = []
+  while (pq.length() !== 0) {
+    got.push(pq.pop()!)
+  }
+
+  expect(got).toStrictEqual(vals)
+})
---
+++ src/util/PriorityQueue.ts
@@ -0,0 +1,126 @@
+type Comparator<T> = (a: T, b: T) => number
+
+/**
+ * A priority queue, backed by a binary heap. The comparator is used to model
+ * a min heap, meaning that lower ranks have higher priority.
+ *
+ * The implementation will grow internal capacity to fit usage, but will not
+ * reclaim capacity as the queue shrinks.
+ */
+export class PriorityQueue<T> {
+  private storage: Array<T> // length of storage is our capacity
+  private _length: number
+  private comparator: Comparator<T>
+
+  public constructor(comparator: Comparator<T>) {
+    this.storage = new Array(64)
+    this._length = 0
+    this.comparator = comparator
+  }
+
+  /**
+   * Insert a new value into the queue. The length will increase by one.
+   */
+  public push(v: T): void {
+    // If we are at capacity, we need to expand internal storage. We'll use the
+    // conventional exponential growth heuristic.
+    if (this.storage.length === this._length) {
+      const newStorage = new Array(this.storage.length * 2)
+      for (const i in this.storage) {
+        newStorage[i] = this.storage[i]
+      }
+      this.storage = newStorage
+    }
+
+    // First, append to storage, breaking the heap property.
+    let vPos = this._length
+    this.storage[vPos] = v
+    this._length += 1
+
+    // Restore the heap property by bubbling up the value.
+    while (vPos > 0) {
+      const parent = Math.floor((vPos - 1) / 2)
+      if (this.comparator(v, this.storage[parent]) >= 0) {
+        // We're done; v is properly situated relative to parent.
+        break
+      }
+
+      // swap parent with child
+      this.storage[vPos] = this.storage[parent]
+      this.storage[parent] = v
+      vPos = parent
+    }
+  }
+
+  /**
+   * Remove the lowest ranking value from the queue, according to the
+   * comparator. The value is returned. The length of the queue will decrease by
+   * one.
+   */
+  public pop(): T | null {
+    if (this._length === 0) {
+      return null
+    }
+
+    const result = this.storage[0]
+
+    // Move the last value to the top of the heap, breaking the heap property.
+    const v = this.storage[this._length - 1]
+    let vPos = 0
+    this.storage[vPos] = v
+    this._length -= 1
+
+    // Push the top value downward, restoring the heap property.
+    while (true) {
+      // We should replace the lowest ranking child whose rank is lower than
+      // the rank of the current value.
+      let swapPos = vPos
+
+      const leftChild = 2 * vPos + 1
+      if (
+        leftChild < this._length &&
+        this.comparator(this.storage[leftChild], this.storage[swapPos]) < 0
+      ) {
+        swapPos = leftChild
+      }
+
+      const rightChild = 2 * vPos + 2
+      if (
+        rightChild < this._length &&
+        this.comparator(this.storage[rightChild], this.storage[swapPos]) < 0
+      ) {
+        swapPos = rightChild
+      }
+
+      if (swapPos === vPos) {
+        // If neither child should be pushed up to the parent position, then
+        // we're done.
+        break
+      }
+
+      this.storage[vPos] = this.storage[swapPos]
+      this.storage[swapPos] = v
+      vPos = swapPos
+    }
+
+    return result
+  }
+
+  /**
+   * Return the lowest ranking value from the queue, according to the comparator.
+   */
+  public peek(): T | null {
+    if (this.storage.length === 0) {
+      return null
+    }
+
+    return this.storage[0]
+  }
+
+  /**
+   * Return the number of values currently in the queue.
+   */
+  public length(): number {
+    return this._length
+  }
+}
