b9764ef9e5f651e615c5756933bdc1e6e0dd1608
Dominic Dagradi <ddagradi@gmail.com>
2020-11-05 20:38:28 -0800

    Hiding is faster

--- src/systems/hiding.ts
+++ src/systems/hiding.ts
@@ -6,19 +6,18 @@ import { aabbOverlapArea } from '~/util/math'
 const REQUIRED_OVERLAP = 0.5
 
 export const update = (entityManager: EntityManager): void => {
-  const obscuringAabbs: [vec2, vec2][] = []
-
-  // TODO: use spatial index to limit the size of this list
-  for (const id of entityManager.obscurings) {
-    const hitbox = entityManager.hitboxes.get(id)!
-    const transform = entityManager.transforms.get(id)!
-    obscuringAabbs.push(hitbox.aabb(transform.position))
-  }
-
   for (const id of entityManager.friendlyTeam) {
     const hitbox = entityManager.hitboxes.get(id)!
     const transform = entityManager.transforms.get(id)!
     const obscurableAabb = hitbox.aabb(transform.position)
+    const obscuringAabbs = entityManager
+      .queryByWorldPos(obscurableAabb)
+      .filter((oid) => entityManager.obscurings.has(oid))
+      .map((oid) => {
+        const obscuringHitbox = entityManager.hitboxes.get(oid)!
+        const obscuringTransform = entityManager.transforms.get(oid)!
+        return obscuringHitbox.aabb(obscuringTransform.position)
+      })
     const checkArea = hitbox.dimensions[0] * hitbox.dimensions[1]
 
     let overlap = 0
