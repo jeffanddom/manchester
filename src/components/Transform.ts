99acb8308b97876d05367e3371bd26985fdfba0d
Jeff Lee <jeffomatic@gmail.com>
2020-11-10 20:09:27 -0800

    transform.clone accepts Immutable transform

--- src/components/Transform.ts
+++ src/components/Transform.ts
@@ -1,4 +1,6 @@
-import { mat2d, vec2 } from 'gl-matrix'
+import { ReadonlyVec2, mat2d, vec2 } from 'gl-matrix'
+
+import { Immutable } from '~/util/Immutable'
 
 export interface Transform {
   previousPosition: vec2
@@ -18,8 +20,8 @@ export const make = (): Transform => ({
   orientation: 0,
 })
 
-export const clone = (src: Transform): Transform => ({
-  previousPosition: vec2.clone(src.previousPosition),
-  position: vec2.clone(src.position),
+export const clone = (src: Immutable<Transform>): Transform => ({
+  previousPosition: vec2.clone(src.previousPosition as ReadonlyVec2),
+  position: vec2.clone(src.position as ReadonlyVec2),
   orientation: src.orientation,
 })
