655f8db0601a7447e38285f663a8796f85e3e50d
Jeff Lee <jeffomatic@gmail.com>
2020-11-13 22:01:55 -0800

    add ComponentSet class

---
+++ src/ComponentSet.test.ts
@@ -0,0 +1,68 @@
+import { ComponentSet } from './ComponentSet'
+
+import { EntityId } from '~/entities/EntityId'
+
+describe('ComponentSet', () => {
+  it('#has', () => {
+    const table = new ComponentSet()
+
+    table.add(0 as EntityId)
+    table.add(1 as EntityId)
+
+    expect(table.has(0 as EntityId)).toBe(true)
+    expect(table.has(1 as EntityId)).toBe(true)
+    expect(table.has(2 as EntityId)).toBe(false)
+  })
+
+  it('#delete', () => {
+    const table = new ComponentSet()
+
+    table.add(0 as EntityId)
+    table.add(1 as EntityId)
+    table.delete(0 as EntityId)
+    table.delete(2 as EntityId) // okay to delete non-existent IDs
+
+    expect(table.has(0 as EntityId)).toBe(false)
+    expect(table.has(1 as EntityId)).toBe(true)
+  })
+
+  it('iteration', () => {
+    const table = new ComponentSet()
+
+    table.add(0 as EntityId)
+    table.add(1 as EntityId)
+    table.add(2 as EntityId)
+
+    expect([...table]).toStrictEqual([
+      0 as EntityId,
+      1 as EntityId,
+      2 as EntityId,
+    ])
+  })
+
+  it('commit/rollback', () => {
+    const table = new ComponentSet()
+
+    table.add(0 as EntityId)
+    expect(table.has(0 as EntityId)).toBe(true)
+
+    table.commit()
+    expect(table.has(0 as EntityId)).toBe(true)
+
+    table.add(1 as EntityId)
+    expect(table.has(1 as EntityId)).toBe(true)
+
+    table.rollback()
+    expect(table.has(0 as EntityId)).toBe(true)
+    expect(table.has(1 as EntityId)).toBe(false)
+
+    table.add(1 as EntityId)
+    expect(table.has(1 as EntityId)).toBe(true)
+
+    table.commit()
+    expect(table.has(1 as EntityId)).toBe(true)
+
+    table.rollback()
+    expect(table.has(1 as EntityId)).toBe(true)
+  })
+})
