4ce037dd57380152c993ffc642ad982d2c64a730
Jeff Lee <jeffomatic@gmail.com>
2021-01-30 00:35:41 -0800

    rendertoy: re-enable view offset control

--- src/tools/rendertoy/Camera.ts
+++ src/tools/rendertoy/Camera.ts
@@ -1,13 +1,15 @@
-import { mat4, vec2, vec3 } from 'gl-matrix'
+import { mat4, quat, vec3 } from 'gl-matrix'
 
 import { MouseButton, mouseButtonsFromBitmask } from '~/input/interfaces'
 import * as math from '~/util/math'
 
 export class Camera {
   private spherePos: math.SphereCoord
+  private viewOffset: vec3
 
   constructor(canvas: HTMLCanvasElement) {
     this.spherePos = math.sphereCoordFromValues(3, Math.PI / 2, 0)
+    this.viewOffset = vec3.create()
 
     // stop right clicks from opening the context menu
     canvas.addEventListener('contextmenu', (e) => e.preventDefault())
@@ -35,6 +37,12 @@ export class Camera {
           Math.PI - 0.0005,
         )
       }
+
+      if (buttons.has(MouseButton.RIGHT)) {
+        const scale = 0.0025
+        this.viewOffset[0] += -event.movementX * scale
+        this.viewOffset[1] += event.movementY * scale
+      }
     })
   }
 
@@ -46,6 +54,16 @@ export class Camera {
       vec3.fromValues(0, 0, 0),
       vec3.fromValues(0, 1, 0),
     )
+    const offset = mat4.fromTranslation(
+      mat4.create(),
+      vec3.transformQuat(
+        vec3.create(),
+        this.viewOffset,
+        mat4.getRotation(quat.create(), m),
+      ),
+    )
+
+    mat4.multiply(m, offset, m)
     return mat4.invert(m, m)
   }
 }
