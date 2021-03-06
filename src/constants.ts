b066200ee0ca0ad6916bad572d6d05ade77c7642
Jeff Lee <jeffomatic@gmail.com>
2021-03-06 06:41:23 +0000

    restore sane constants

--- src/constants.ts
+++ src/constants.ts
@@ -1,6 +1,6 @@
 export const TILE_SIZE = 1
 export const SIMULATION_PERIOD_S = 1 / 60
 export const MAX_PREDICTED_FRAMES = 12
-export const PLAYER_COUNT = 2
+export const PLAYER_COUNT = 1
 export const SERVER_PORT = 3000
-export const CLIENT_INPUT_DELAY = 20
+export const CLIENT_INPUT_DELAY = 3
