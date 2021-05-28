629d6eda21a36021395c84757d5e955b7b62d47f
Dominic Dagradi <ddagradi@gmail.com>
2021-05-27 19:26:20 -0700

    Session 99: 22 Jump Point Search

--- src/util/__test__/jps.test.ts
+++ src/util/__test__/jps.test.ts
@@ -1,6 +1,48 @@
-import { getNeighbors } from '../jps'
+import { getNeighbors, jps } from '../jps'
 
 describe('jps', () => {
+  describe('full search', () => {
+    it('finds a path with no obstacles', () => {
+      // prettier-ignore
+      const grid = new Uint8Array([
+        0, 0, 0,
+        0, 0, 0,
+        0, 0, 0
+      ])
+
+      const path = jps(0, 0, 2, 2, grid, 3)
+      expect(path).toEqual([0, 0, 2, 2])
+    })
+
+    it('finds a path with one obstacles', () => {
+      // prettier-ignore
+      const grid = new Uint8Array([
+        0, 0, 0,
+        0, 1, 0,
+        0, 0, 0
+      ])
+
+      const path = jps(0, 0, 2, 2, grid, 3)
+      expect(path).toEqual([0, 0, 0, 1, 1, 2, 2, 2])
+    })
+
+    it('finds a path in a larger map', () => {
+      // prettier-ignore
+      const grid = new Uint8Array([
+        0, 0, 0, 0, 0,
+        0, 0, 1, 0, 0,
+        0, 0, 1, 0, 0,
+        0, 0, 1, 0, 0,
+        0, 0, 0, 0, 0
+      ])
+
+      const path = jps(0, 2, 4, 2, grid, 5)
+      expect(path).toEqual([0, 2, 2, 0, 4, 2])
+    })
+  })
+
+  // describe('findJump', () => { })
+
   describe('getNeighbors', () => {
     it('no forced neighbors', () => {
       // prettier-ignore
@@ -10,34 +52,42 @@ describe('jps', () => {
         0, 0, 0,
       ])
 
-      expect(getNeighbors(1, 1, 1, 0, grid, 3)).toEqual([2, 1, 0])
-      expect(getNeighbors(1, 1, -1, 0, grid, 3)).toEqual([0, 1, 0])
-      expect(getNeighbors(1, 1, 0, 1, grid, 3)).toEqual([1, 2, 0])
-      expect(getNeighbors(1, 1, 0, -1, grid, 3)).toEqual([1, 0, 0])
+      // prettier-ignore
+      expect(getNeighbors({ x: 1, y: 1, dirX: 1, dirY: 0 }, grid, 3)).toEqual(
+        [{ x: 2, y: 1, dirX: 1, dirY: 0 }])
+      // prettier-ignore
+      expect(getNeighbors({ x: 1, y: 1, dirX: -1, dirY: 0 }, grid, 3)).toEqual(
+        [{ x: 0, y: 1, dirX: -1, dirY: 0 }])
+      // prettier-ignore
+      expect(getNeighbors({ x: 1, y: 1, dirX: 0, dirY: 1 }, grid, 3)).toEqual(
+        [{ x: 1, y: 2, dirX: 0, dirY: 1 }])
+      // prettier-ignore
+      expect(getNeighbors({ x: 1, y: 1, dirX: 0, dirY: -1 }, grid, 3)).toEqual(
+        [{ x: 1, y: 0, dirX: 0, dirY: -1 }])
 
       // prettier-ignore
-      expect(getNeighbors(1, 1, 1, 1, grid, 3)).toEqual([
-        2, 2, 0,
-        2, 1, 0,
-        1, 2, 0,
+      expect(getNeighbors({ x: 1, y: 1, dirX: 1, dirY: 1 }, grid, 3)).toEqual([
+        { x: 2, y: 2, dirX: 1, dirY: 1 },
+        { x: 2, y: 1, dirX: 1, dirY: 0 },
+        { x: 1, y: 2, dirX: 0, dirY: 1 },
       ])
       // prettier-ignore
-      expect(getNeighbors(1, 1, -1, -1, grid, 3)).toEqual([
-        0, 0, 0,
-        0, 1, 0,
-        1, 0, 0,
+      expect(getNeighbors({ x: 1, y: 1, dirX: -1, dirY: -1 }, grid, 3)).toEqual([
+        { x: 0, y: 0, dirX: -1, dirY: -1 },
+        { x: 0, y: 1, dirX: -1, dirY: 0 },
+        { x: 1, y: 0, dirX: 0, dirY: -1 },
       ])
       // prettier-ignore
-      expect(getNeighbors(1, 1, -1, 1, grid, 3)).toEqual([
-        0, 2, 0,
-        0, 1, 0,
-        1, 2, 0,
+      expect(getNeighbors({ x: 1, y: 1, dirX: -1, dirY: 1 }, grid, 3)).toEqual([
+        { x: 0, y: 2, dirX: -1, dirY: 1 },
+        { x: 0, y: 1, dirX: -1, dirY: 0 },
+        { x: 1, y: 2, dirX: 0, dirY: 1 },
       ])
       // prettier-ignore
-      expect(getNeighbors(1, 1, 1, -1, grid, 3)).toEqual([
-        2, 0, 0,
-        2, 1, 0,
-        1, 0, 0,
+      expect(getNeighbors({ x: 1, y: 1, dirX: 1, dirY: -1 }, grid, 3)).toEqual([
+        { x: 2, y: 0, dirX: 1, dirY: -1 },
+        { x: 2, y: 1, dirX: 1, dirY: 0 },
+        { x: 1, y: 0, dirX: 0, dirY: -1 },
       ])
     })
 
@@ -50,25 +100,29 @@ describe('jps', () => {
           0, 0, 0,
         ])
 
-        expect(getNeighbors(1, 1, 1, 0, grid, 3)).toEqual([2, 1, 0])
-        expect(getNeighbors(1, 1, -1, 0, grid, 3)).toEqual([])
         // prettier-ignore
-        expect(getNeighbors(1, 1, 0, 1, grid, 3)).toEqual([
-          1, 2, 0,
-          0, 2, 1
+        expect(getNeighbors({ x: 1, y: 1, dirX: 1, dirY: 0 }, grid, 3)).toEqual(
+          [{ x: 2, y: 1, dirX: 1, dirY: 0 }])
+        expect(
+          getNeighbors({ x: 1, y: 1, dirX: -1, dirY: 0 }, grid, 3),
+        ).toEqual([])
+        // prettier-ignore
+        expect(getNeighbors({ x: 1, y: 1, dirX: 0, dirY: 1 }, grid, 3)).toEqual([
+          { x: 1, y: 2, dirX: 0, dirY: 1 },
+          { x: 0, y: 2, dirX: -1, dirY: 1, forcedNeighbor: true }
         ])
         // prettier-ignore
-        expect(getNeighbors(1, 1, 0, -1, grid, 3)).toEqual([
-          1, 0, 0,
-          0, 0, 1
+        expect(getNeighbors({ x: 1, y: 1, dirX: 0, dirY: -1 }, grid, 3)).toEqual([
+          { x: 1, y: 0, dirX: 0, dirY: -1 },
+          { x: 0, y: 0, dirX: -1, dirY: -1, forcedNeighbor: true }
         ])
 
         // prettier-ignore
-        expect(getNeighbors(1, 1, 1, -1, grid, 3)).toEqual([
-          2, 0, 0,
-          2, 1, 0,
-          1, 0, 0,
-          0, 0, 1,
+        expect(getNeighbors({ x: 1, y: 1, dirX: 1, dirY: -1 }, grid, 3)).toEqual([
+          { x: 2, y: 0, dirX: 1, dirY: -1 },
+          { x: 2, y: 1, dirX: 1, dirY: 0 },
+          { x: 1, y: 0, dirX: 0, dirY: -1 },
+          { x: 0, y: 0, dirX: -1, dirY: -1, forcedNeighbor: true },
         ])
       })
 
@@ -81,12 +135,12 @@ describe('jps', () => {
         ])
 
         // prettier-ignore
-        expect(getNeighbors(1, 1, 1, -1, grid, 3)).toEqual([
-          2, 0, 0,
-          2, 1, 0,
-          1, 0, 0,
-          0, 0, 1,
-          2, 2, 1,
+        expect(getNeighbors({ x: 1, y: 1, dirX: 1, dirY: -1 }, grid, 3)).toEqual([
+          { x: 2, y: 0, dirX: 1, dirY: -1 },
+          { x: 2, y: 1, dirX: 1, dirY: 0 },
+          { x: 1, y: 0, dirX: 0, dirY: -1 },
+          { x: 0, y: 0, dirX: -1, dirY: -1, forcedNeighbor: true },
+          { x: 2, y: 2, dirX: 1, dirY: 1, forcedNeighbor: true },
         ])
       })
     })
--- src/util/jps.ts
+++ src/util/jps.ts
@@ -1,113 +1,271 @@
-// export function jps(
-//   sx: number,
-//   sy: number,
-//   dx: number,
-//   dy: number,
-//   grid: Uint8Array,
-//   w: number,
-// ): number[] {}
-//
-//
-/*
-while openList has stuff:
-  current = openList.getHighestPri()
-  for -1..1 as stepX:
-    for -1..1 as stepY:
-      next if stepX is 0 and stepY is 0
-      foreach findJump(current.x, current.y, stepX, stepY, grid, w) as jumpPoint:
-        openList.push(jumpPoint)
+import { PriorityQueue } from '~/util/PriorityQueue'
 
-findJump(
+type SearchPoint = {
+  x: number
+  y: number
+  dirX: number
+  dirY: number
+  forcedNeighbor?: boolean
+  previous?: string
+  distance?: number
+}
+
+function pointId(point: SearchPoint): string {
+  return `${point.x},${point.y}`
+}
+
+function chebyshev(x1: number, y1: number, x2: number, y2: number): number {
+  return Math.max(Math.abs(x1 - x2), Math.abs(y1 - y2))
+}
+
+function inBounds(x: number, y: number, grid: Uint8Array, w: number): boolean {
+  return 0 <= x && x < w && 0 <= y && y < grid.length / w
+}
+
+function validWall(x: number, y: number, grid: Uint8Array, w: number): boolean {
+  return inBounds(x, y, grid, w) && grid[y * w + x] === 1
+}
+
+function validPoint(
   x: number,
   y: number,
-  stepX: -1 | 0 | 1,
-  stepY: -1 | 0 | 1,
   grid: Uint8Array,
-  w: number
-):
-  nextNode = x,y + stepX,stepY
+  w: number,
+): boolean {
+  return inBounds(x, y, grid, w) && grid[y * w + x] !== 1
+}
 
-  if (nextNode is wall or out of bounds)
+// TODO:
+// - distance should account for path length walked + distance to destination
+// - chebyshev replaced with euclidian
+// - prevent walking through diagonal "gaps"
+// - what happens if you push the same node twice onto the open list?
+//     if different directions, allow it.
+//     if not, ??? (that seems technically impossible)
+
+export function jps(
+  sx: number,
+  sy: number,
+  dx: number,
+  dy: number,
+  grid: Uint8Array,
+  w: number,
+): number[] {
+  const pathNodes = new Map<string, SearchPoint>()
+  const openList = new PriorityQueue<SearchPoint>(
+    (a, b) => a.distance! - b.distance!,
+  )
+
+  // Initial setup
+  const start = {
+    x: sx,
+    y: sy,
+    dirX: 0,
+    dirY: 0,
+    distance: chebyshev(sx, sy, dx, dy),
+  }
+  pathNodes.set(pointId(start), start)
+  openList.push(start)
+
+  // Search from open nodes
+  while (openList.length() > 0) {
+    const current = openList.pop()
+    if (current === undefined) {
+      return []
+    }
+
+    if (current.x === dx && current.y === dy) {
+      break
+    }
+
+    const checkSet =
+      current.previous === undefined
+        ? getImmediateNeighbors(current, grid, w)
+        : getNeighbors(current, grid, w)
+
+    for (const point of checkSet) {
+      const jumpPoint = findJump(point, point.dirX, point.dirY, dx, dy, grid, w)
+      if (jumpPoint !== null) {
+        const distance = chebyshev(jumpPoint.x, jumpPoint.y, dx, dy)
+
+        const toAdd: SearchPoint = {
+          ...jumpPoint,
+          distance: distance,
+          previous: pointId(current),
+        }
+
+        openList.push(toAdd)
+        pathNodes.set(pointId(toAdd), toAdd)
+      }
+    }
+  }
+
+  const path = []
+  let nextNode = pathNodes.get(`${dx},${dy}`)
+  while (nextNode !== undefined) {
+    path.unshift(nextNode.x, nextNode.y)
+    nextNode = pathNodes.get(nextNode.previous!)
+  }
+
+  return path
+}
+
+export function findJump(
+  point: SearchPoint,
+  stepX: number,
+  stepY: number,
+  dx: number,
+  dy: number,
+  grid: Uint8Array,
+  w: number,
+): SearchPoint | null {
+  // Goal! Return the node
+  if (point.x === dx && point.y === dy) {
+    return point
+  }
+
+  // Node is a jump point, return the jump point
+  const neighbors = getNeighbors(point, grid, w)
+  if (neighbors.find((p) => p.forcedNeighbor) !== undefined) {
+    return point
+  }
+
+  const nextX = point.x + stepX
+  const nextY = point.y + stepY
+
+  if (!validPoint(nextX, nextY, grid, w)) {
     return null
+  }
 
-  if (nextNode is goal)
-    return nextNode
+  const nextPoint = {
+    x: nextX,
+    y: nextY,
+    dirX: stepX,
+    dirY: stepY,
+    distance: chebyshev(nextX, nextY, dx, dy),
+  }
 
-  if hasForcedNeighbors(getNeighbors(nextNode, stepX, stepY, grid, w)):
-    return nextNode    
-  
-  if diagonal:
-    a = findJump(nextNode.x, nextNode.y, 0, stepY, grid, w) if the thing is not null
-    b = findJump(nextNode.x, nextNode.y, stepX, 0, grid, w) if the thing is not null
-    if a && b
-      return [a, b]
-    else if a
-      return [a]
-    else if b
-      return [b]
+  // Check diagonals
+  if (stepX !== 0 && stepY !== 0) {
+    const diagonal1 = findJump(nextPoint, stepX, 0, dx, dy, grid, w)
+    if (diagonal1 !== null) {
+      return diagonal1
+    }
+    const diagonal2 = findJump(nextPoint, 0, stepY, dx, dy, grid, w)
+    if (diagonal2 !== null) {
+      return diagonal2
+    }
+  }
 
-  return findJump(nextNode.x, nextNode.y, stepX, stepY, grid, w)
-*/
+  return findJump(nextPoint, stepX, stepY, dx, dy, grid, w)
+}
 
 /**
- * Returns a flattened array of (x, y, forced) tuples. `forced` is 1 if the
+ * Returns a flattened array of (x, y, dirX, dirY, forced) tuples. `forced` is 1 if the
  * neighbor is a forced neighbor.
  */
 export function getNeighbors(
-  x: number,
-  y: number,
-  stepX: -1 | 0 | 1,
-  stepY: -1 | 0 | 1,
+  point: SearchPoint,
   grid: Uint8Array,
   w: number,
-): number[] {
-  return stepX !== 0 && stepY !== 0
-    ? getNeighborsDiagonal(x, y, stepX, stepY, grid, w)
-    : getNeighborsCardinal(x, y, stepX, stepY, grid, w)
+): SearchPoint[] {
+  return point.dirX !== 0 && point.dirY !== 0
+    ? getNeighborsDiagonal(point, grid, w)
+    : getNeighborsCardinal(point, grid, w)
+}
+
+const potentialSteps = [-1, 0, 1]
+function getImmediateNeighbors(
+  point: SearchPoint,
+  grid: Uint8Array,
+  w: number,
+) {
+  const res: SearchPoint[] = []
+
+  for (const stepX of potentialSteps) {
+    for (const stepY of potentialSteps) {
+      if (stepX === 0 && stepY === 0) {
+        continue
+      }
+
+      const newX = point.x + stepX
+      const newY = point.y + stepY
+      if (!validPoint(newX, newY, grid, w)) {
+        continue
+      }
+
+      res.push({
+        x: newX,
+        y: newY,
+        dirX: stepX,
+        dirY: stepY,
+      })
+    }
+  }
+
+  return res
 }
 
 function getNeighborsDiagonal(
-  x: number,
-  y: number,
-  stepX: -1 | 0 | 1,
-  stepY: -1 | 0 | 1,
+  point: SearchPoint,
   grid: Uint8Array,
   w: number,
-): number[] {
-  const res: number[] = []
+): SearchPoint[] {
+  const { x, y, dirX, dirY } = point
+  const res: SearchPoint[] = []
 
   // Check unforced neighbors
 
-  const p1 = (y + stepY) * w + (x + stepX)
-  if (grid[p1] === 0) {
-    res.push(x + stepX, y + stepY, 0)
+  if (validPoint(x + dirX, y + dirY, grid, w)) {
+    res.push({
+      x: x + dirX,
+      y: y + dirY,
+      dirX: dirX,
+      dirY: dirY,
+    })
   }
 
-  const p2 = y * w + (x + stepX)
-  if (grid[p2] === 0) {
-    res.push(x + stepX, y, 0)
+  if (validPoint(x + dirX, y, grid, w)) {
+    res.push({
+      x: x + dirX,
+      y: y,
+      dirX: dirX,
+      dirY: 0,
+    })
   }
 
-  const p3 = (y + stepY) * w + x
-  if (grid[p3] === 0) {
-    res.push(x, y + stepY, 0)
+  if (validPoint(x, y + dirY, grid, w)) {
+    res.push({
+      x: x,
+      y: y + dirY,
+      dirX: 0,
+      dirY: dirY,
+    })
   }
 
   // Check forced neighbors
 
-  const p4 = y * w + (x - stepX)
-  if (grid[p4] === 1) {
-    const n = (y + stepY) * w + (x - stepX)
-    if (grid[n] === 0) {
-      res.push(x - stepX, y + stepY, 1)
+  if (validWall(x - dirX, y, grid, w)) {
+    if (validPoint(x - dirX, y + dirY, grid, w)) {
+      res.push({
+        x: x - dirX,
+        y: y + dirY,
+        dirX: -dirX,
+        dirY: dirY,
+        forcedNeighbor: true,
+      })
     }
   }
 
-  const p5 = (y - stepY) * w + x
-  if (grid[p5] === 1) {
-    const n = (y - stepY) * w + (x + stepX)
-    if (grid[n] === 0) {
-      res.push(x + stepX, y - stepY, 1)
+  if (validWall(x, y - dirY, grid, w)) {
+    if (validPoint(x + dirX, y - dirY, grid, w)) {
+      res.push({
+        x: x + dirX,
+        y: y - dirY,
+        dirX: dirX,
+        dirY: -dirY,
+        forcedNeighbor: true,
+      })
     }
   }
 
@@ -115,54 +273,81 @@ function getNeighborsDiagonal(
 }
 
 function getNeighborsCardinal(
-  x: number,
-  y: number,
-  stepX: -1 | 0 | 1,
-  stepY: -1 | 0 | 1,
+  point: SearchPoint,
   grid: Uint8Array,
   w: number,
-): number[] {
-  const res: number[] = []
+): SearchPoint[] {
+  const { x, y, dirX, dirY } = point
+  const res: SearchPoint[] = []
 
   // Check unforced neighbors
-  const p = (y + stepY) * w + (x + stepX)
+  const p = (y + dirY) * w + (x + dirX)
   if (grid[p] === 0) {
-    res.push(x + stepX, y + stepY, 0)
+    res.push({
+      x: x + dirX,
+      y: y + dirY,
+      dirX: dirX,
+      dirY: dirY,
+    })
   }
 
   // Forced neighbor check
-  if (stepY === 0) {
+  if (dirY === 0) {
     // horizontal
     const p1 = (y + 1) * w + x
     if (grid[p1] === 1) {
-      const n = (y + 1) * w + (x + stepX)
+      const n = (y + 1) * w + (x + dirX)
       if (grid[n] === 0) {
-        res.push(x + stepX, y + 1, 1)
+        res.push({
+          x: x + dirX,
+          y: y + 1,
+          dirX: dirX,
+          dirY: 1,
+          forcedNeighbor: true,
+        })
       }
     }
 
     const p2 = (y - 1) * w + x
     if (grid[p2] === 1) {
-      const n = (y - 1) * w + (x + stepX)
+      const n = (y - 1) * w + (x + dirX)
       if (grid[n] === 0) {
-        res.push(x + stepX, y - 1, 1)
+        res.push({
+          x: x + dirX,
+          y: y - 1,
+          dirX: dirX,
+          dirY: -1,
+          forcedNeighbor: true,
+        })
       }
     }
   } else {
     // vertical
     const p1 = y * w + (x + 1)
     if (grid[p1] === 1) {
-      const n = (y + stepY) * w + (x + 1)
+      const n = (y + dirY) * w + (x + 1)
       if (grid[n] === 0) {
-        res.push(x + 1, y + stepY, 1)
+        res.push({
+          x: x + 1,
+          y: y + dirY,
+          dirX: 1,
+          dirY: dirY,
+          forcedNeighbor: true,
+        })
       }
     }
 
     const p2 = y * w + (x - 1)
     if (grid[p2] === 1) {
-      const n = (y + stepY) * w + (x - 1)
+      const n = (y + dirY) * w + (x - 1)
       if (grid[n] === 0) {
-        res.push(x - 1, y + stepY, 1)
+        res.push({
+          x: x - 1,
+          y: y + dirY,
+          dirX: -1,
+          dirY: dirY,
+          forcedNeighbor: true,
+        })
       }
     }
   }
